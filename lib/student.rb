15f44949de9d5235fceafafde01abc30b4f3586d
github username <github email address>
2018-08-14 18:13:12 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,67 @@
+require 'pry'
 class Student
-
+  attr_accessor :name, :grade
+  attr_reader  :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+
+  def initialize (name, grade, id: nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.create(hash)
+    # sql = <<-SQL
+    #   INSERT INTO students (name, grade) VALUES (?, ?)
+    # SQL
+
+    # DB[:conn].execute(sql, hash[:name], hash[:grade])
+    # sql_lookup = <<-SQL
+    #   SELECT * FROM students ORDER BY id DESC LIMIT 1
+    # SQL
+    # info = DB[:conn].execute(sql_lookup)[0]
+    student = Student.new(hash[:name], hash[:grade])
+    student.save
+    student
+    # binding.pry
+    # puts 'nope'
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
   
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    sql_student = <<-SQL
+      SELECT * FROM students ORDER BY id DESC LIMIT 1
+    SQL
+
+    info = DB[:conn].execute(sql_student)
+    # binding.pry
+    @id = info[0][0]
+  end
+
 end
